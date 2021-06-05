ea26c684c74d6b8c7a2cdb4a0abe7939bb4d7cda
safafa <safaaballagh97@gmail.com>
2021-06-05 20:28:26 +0100

    Add views methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,48 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
-      end
+        
+    end
     
-      def create
+    def create
         @user = User.new(user_params)
     
         if @user.save
-          redirect_to new_user_path
+          redirect_to root_path
         else
           render :new
         end
-      end
+    end
+
+    def edit
+       @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+
+        redirect_to root_path
+
+    end
 
       private
   def user_params
